3adce36fd0e94d4d380126c6e5ae72f472a0dcf8
sonnie36 <sonniemwati36@gmail.com>
2024-04-19 09:44:50 -0700

    Added Explore menu

---
+++ src/Components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './ExploreMenu.css'
+import {menu_list} from '../../assets/assets'
+
+export const ExploreMenu = () => {
+  return (
+    <div className='explore-menu' id="explore-menu">
+        <h1>Explore our menu</h1>
+        <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array</p>
+        <div className='explore-menu-list'>
+            {
+              menu_list.map((item, index)=>{
+                return(
+                    <div key={index} className="explore-menu-list-item">
+                        <img src={item.menu_image} alt="" />
+                        <p>{item.menu_name}</p>
+                    </div>
+                )
+              })
+            }
+        </div>
+    </div>
+  )
+}
